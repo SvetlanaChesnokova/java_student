9815054d2ae3dcc13f026bf9fa15bffc8b3e7651
SvetlanaChesnokova <s.mozgunova@ngs.ru>
2016-10-28 00:08:57 +0700

    Урок 1 № 14, конструктор

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Rectangle.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Светлана on 27.10.2016.
+ */
+public class Rectangle {
+
+  public double a;
+  public double b;
+
+  public Rectangle (double a, double b) {
+    this.a = a;
+    this.b = b;
+  }
+}
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,13 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Светлана on 27.10.2016.
+ */
+public class Square {
+
+ public double l;
+
+ public Square(double l) {
+   this.l = l;
+ }
+}
