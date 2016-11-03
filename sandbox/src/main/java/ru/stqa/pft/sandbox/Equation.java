1152dc0702335df9b4dd64e906f54b55e6fe12ab
SvetlanaChesnokova <s.mozgunova@ngs.ru>
2016-11-03 20:12:19 +0700

    Проверки if с вложениями

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
@@ -19,16 +19,52 @@ public class Equation {
 
     double d = b * b - 4 * a * c;
 
-    if (d > 0) {
-      n = 2;
+   /* //оставила для себя
+    if (a == 0){
+    //Вывод текста на экран
+      System.out.println("Это вырожденное уравнение");
+    }*/
+
+    if (a != 0) {
+     if (d > 0){
+       n = 2;
+     } else if (d == 0) {
+       n = 1;
+     } else {
+       n = 0;
+     }
+
+    } else  if (b != 0) {
+      n = 1;
+
+    } else if (c != 0) {
+      n = 0;
+
     } else {
-      if (d == 0) {
+      n = -1;
+    }
+
+   //меняем порядок выполнения проверки
+   /* if (a == 0) {
+      if (b == 0) {
+        if (c == 0) {
+          n = -1;
+        } else {
+          n = 0;
+        }
+      } else {
+        n = 1;
+      }
+
+    } else{
+      if (d > 0) {
+        n = 2;
+      } else if (d == 0) {
         n = 1;
       } else {
         n = 0;
       }
-    }
-
+    }*/
 
   }
 
