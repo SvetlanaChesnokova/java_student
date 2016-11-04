75e5f076a36c1df04688b4be5516aa462c9c18be
SvetlanaChesnokova <s.mozgunova@ngs.ru>
2016-11-04 12:15:06 +0700

    Запуск тестов в разных браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
@@ -8,8 +9,11 @@ import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
  * Created by Светлана on 01.11.2016.
  */
 public class TestBase {
-
-  protected final ApplicationManager app = new ApplicationManager();
+  // Выбор браузера для запусков тестов
+  // драйверы для запуска в папке C:\tools
+  //protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+ // protected final ApplicationManager app = new ApplicationManager(BrowserType.IE);
 
   @BeforeMethod
   public void setUp() throws Exception {
