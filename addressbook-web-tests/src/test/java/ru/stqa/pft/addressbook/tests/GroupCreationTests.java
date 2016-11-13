a17643cc85fe9846493656fb9622b57b3c24f624
SvetlanaChesnokova <s.mozgunova@ngs.ru>
2016-11-14 00:05:13 +0700

    Сортировка списков групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -13,48 +14,23 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() {
     //тест для создания группы
     app.getNavigationHelper().gotoGroupPage();
-    //подсчет кол-ва групп (строк) до добавления
-  //  int before = app.getGroupHelper().getGroupCount();
-    //для сравнения размера списка до собавления записаи
+    //для сравнения размера списка до добавления записаи
     List<GroupData> before = app.getGroupHelper().getGroupList();
     GroupData group =new GroupData("test177", "test2", "test3");
-    //GroupData group =new GroupData(before.get(before.size()-1).getId(),"test1", "test2", "test3");
     app.getGroupHelper().createGroup(group);
-    //подсчет кол-ва групп (строк) после добавления
-    //int after = app.getGroupHelper().getGroupCount();
     //для сравнения размера списка после собавления записаи
     List<GroupData> after = app.getGroupHelper().getGroupList();
     //проверка, сравнение
-   // Assert.assertEquals(after , before+1);
     Assert.assertEquals(after.size() , before.size()+1);
 
     //сравнеие списков построчно целиком, как задам в шаблоне equals(Object o) , toString,  hashCode() в  листе GroupData
-    //первый способ сравнения ID
-    int max =0;
-    for (GroupData g : after){
-      if (g.getId() > max) {
-        max = g.getId();
-      }
-    }
-    //второй способ сравнения ID
-    //поток для сравнения, с использованием лямдо выражением
- //   Comparator<? super GroupData> byId = (Comparator<GroupData>) (o1, o2) -> Integer.compare(o1.getId(), o2.getId());
-    /*
-     //поток для сравнения, с использованием анонимного класса - реализация прям тут
-      Comparator<? super GroupData> byId = new Comparator<GroupData>() {
-        @Override
-      public int compare(GroupData o1, GroupData o2) {
-        return Integer.compare(o1.getId(), o2.getId());
-      }
-    };
 
-    int max1 = after.stream().max(byId).get().getId();
-    group.setId(max1);
-     */
-    //список переводим в поток, далее сравниваем и находим мах элемент по id
-    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
-    Assert.assertEquals(new HashSet<Object>(before) , new HashSet<Object>(after));
+    //список переводим в поток, далее сравниваем и находим мах элемент по id
+    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(),g2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before , after);
 
     app.getNavigationHelper().gotoHomePage();
   }
