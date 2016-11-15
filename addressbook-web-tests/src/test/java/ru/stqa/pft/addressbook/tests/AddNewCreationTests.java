4401573d9e67de78ab43b709bda8247f3f96580c
SvetlanaChesnokova <s.mozgunova@ngs.ru>
2016-11-16 00:43:14 +0700

    Fluent-интерфейсы, создание контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddNewCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddNewCreationTests.java
@@ -4,7 +4,6 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ClientData;
 
-import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -14,25 +13,21 @@ public class AddNewCreationTests extends TestBase {
     public void testAddNewCreation() {
         //тест для создания контакта
         //подсчет кол-ва строк до добавления
-        //int before = app.getClientHelper().getClientCount();
-        List<ClientData> before = app.getClientHelper().getClientList();
-        ClientData contakt =  new ClientData("Sidorov8", "Nikolai", "RF, NSK", "+72589631478", "3-147-258@", "Nikolai@tre", "Sidorov@erw.ru", "357-1598", "test1");
-        //app.getClientHelper().createClient(new ClientData("Sidorov", "Nikolai", "RF, NSK", "+72589631478", "3-147-258@", "Nikolai@tre", "Sidorov@erw.ru", "357-1598", "test1"));
-        app.getClientHelper().createClient(contakt);
+        List<ClientData> before = app.contakt().list();
+        ClientData contakt =  new ClientData().withP_firstnam("Sidorov8").withP_lastname("Nikolai").withP_address("RF, NSK")
+                .withP_homepage("+72589631478").withP_email("3-147-258@").withP_email2("Nikolai@tre")
+                .withP_email3("Sidorov@erw.ru").withP_phones("357-1598").withGroup("test1");
+        app.contakt().create(contakt);
         //подсчет кол-ва групп (строк) после добавления
-        //int after = app.getClientHelper().getClientCount();
-        List<ClientData> after = app.getClientHelper().getClientList();
+        List<ClientData> after = app.contakt().list();
         //проверка, сравнение
-        // Assert.assertEquals(after , before+1);
         Assert.assertEquals(after.size(), before.size() + 1);
-
 
         //сравнеие списков построчно целиком, как задам в шаблоне equals(Object o) , toString,  hashCode() в  листе GroupData
 
         contakt.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(contakt);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
-
 
     }
 
