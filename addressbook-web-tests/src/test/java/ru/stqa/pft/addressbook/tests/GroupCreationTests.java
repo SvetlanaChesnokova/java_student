2cd2380afc4953d04faa60543fdba924c46b9f44
chesnokova <chesnokova@sibvaleo.com>
2016-11-19 22:06:24 +0700

    ПИУ1

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,8 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.Set;
 
 public class GroupCreationTests extends TestBase {
 
@@ -14,24 +13,21 @@ public class GroupCreationTests extends TestBase {
     //тест для создания группы
     app.goTo().groupPage();
     //для сравнения размера списка до добавления записаи
-    List<GroupData> before = app.group().list();
+    Set<GroupData> before = app.group().all();
     GroupData group =new GroupData().withName ("test17");
     app.group().create(group);
     //для сравнения размера списка после собавления записаи
-    List<GroupData> after = app.group().list();
+    Set<GroupData> after = app.group().all();
     //проверка, сравнение
     Assert.assertEquals(after.size() , before.size()+1);
 
     //сравнеие списков построчно целиком, как задам в шаблоне equals(Object o) , toString,  hashCode() в  листе GroupData
 
+    group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt());
     before.add(group);
-    //список переводим в поток, далее сравниваем и находим мах элемент по id
-    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(),g2.getId());
-    before.sort(byId);
-    after.sort(byId);
     Assert.assertEquals(before , after);
 
-    app.goTo().gotoHomePage();
+    //app.goTo().gotoHomePage();
   }
 
 }
