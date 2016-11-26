6c23c073322f99361e2ded05f9f95dbbc314706f
chesnokova <chesnokova@sibvaleo.com>
2016-11-26 14:46:17 +0700

    Анализ опций командой строки при запуске генератора данных

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -1,5 +1,8 @@
 package ru.stqa.pft.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.io.File;
@@ -14,18 +17,34 @@ import java.util.List;
  */
 public class GroupDataGenerator {
 
+    @Parameter (names = "-c", description = "Group count")
+    public int count;
+
+    @Parameter (names = "-f", description = "Target file")
+    public String file;
+
     //генерация тестовых данных
     //настройки, кол-во и путь можно указать в Edit Configuration
 
     public static void main (String[] args) throws IOException {
-        int count = Integer.parseInt(args [0]);
-        File file = new File(args [1]);
-
-        List<GroupData> groups = geberateGroups(count);
-        save(groups, file);
+        GroupDataGenerator generator = new GroupDataGenerator();
+        //generator- заполнено опцией, args-передано в командной строке
+        JCommander jCommander = new JCommander(generator);
+        try {
+            jCommander.parse(args);
+        } catch (ParameterException ex){
+            jCommander.usage();
+            return;
+        }
+        generator.run();
     }
 
-    private static void save(List<GroupData> groups, File file) throws IOException {
+    private void run() throws IOException {
+        List<GroupData> groups = geberateGroups(count);
+        save(groups, new File (file));
+    }
+
+    private void save(List<GroupData> groups, File file) throws IOException {
         System.out.println("**************************");
         System.out.println(new File(".").getAbsolutePath());
         System.out.println("**************************");
@@ -37,7 +56,7 @@ public class GroupDataGenerator {
         writer.close();
     }
 
-    private static List<GroupData> geberateGroups(int count) {
+    private List<GroupData> geberateGroups(int count) {
         List<GroupData> groups = new ArrayList<GroupData>();
         for (int i = 0; i < count; i++){
             groups.add(new GroupData().withName(String.format("test %s", i))
