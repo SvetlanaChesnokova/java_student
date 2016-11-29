bc5d0b8703992677b60eb5cd389184f0e9caa74a
chesnokova <chesnokova@sibvaleo.com>
2016-11-30 02:34:43 +0700

    Драйвер базы данных. Получение данных из БД

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DbConnectionTest.java
@@ -0,0 +1,46 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
+
+import java.sql.*;
+
+/**
+ * Created by chesnokova.sa on 30.11.2016.
+ */
+public class DbConnectionTest {
+    @Test
+    public void testDbConnection(){
+        Connection conn = null;
+
+        try {
+            //:3306 -порт ; addressbook - название базы; user= &password=  -для соединения с базой
+            conn =
+                    DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?user=root&password=");
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("select group_id, group_name, group_header, group_footer from group_list");
+            Groups groups = new Groups();
+            while (rs.next()){
+               groups.add(new GroupData().withId(rs.getInt("group_id")).withName(rs.getString("group_name"))
+                        .withHeader(rs.getString("group_header")).withFooter(rs.getString("group_footer")));
+            }
+            //  закрывает память
+            rs.close();
+            // не будем больше выполнять запросы
+            st.close();
+            //закрываем соединение с БД
+            conn.close();
+
+            System.out.println(groups);
+
+            // Do something with the Connection
+
+        } catch (SQLException ex) {
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+    }
+}
