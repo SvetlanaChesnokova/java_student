90e6748f2cdd5c62c3dcc59be16808b20ae4d4c7
chesnokova <chesnokova@sibvaleo.com>
2016-12-05 02:58:30 +0700

    Добавлен новый модуль для тестов для mantis

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -0,0 +1,56 @@
+package ru.stqa.pft.mantis.appmanager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Objects;
+import java.util.Properties;
+
+
+public class ApplicationManager {
+  private final Properties properties;
+  WebDriver wd;
+
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+    properties = new Properties();
+  }
+
+  public void init() throws IOException{
+    String target = System.getProperty("target", "local");
+       properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+   // String browser = BrowserType.FIREFOX;
+    if (Objects.equals(browser, BrowserType.FIREFOX)) {
+      wd = new FirefoxDriver();
+    } else if (Objects.equals(browser, BrowserType.CHROME)) {
+      wd = new ChromeDriver();
+    } else if (Objects.equals(browser, BrowserType.IE)) {
+      wd = new InternetExplorerDriver();
+    }
+    //Ожидание всех элементов на форме, касательно всего проекта
+   // wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+   // wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+    //DriverWait, явные ожидание конкретного элемента с заданным временем в секундах
+   // WebDriverWait wait = new WebDriverWait(wd, 10);
+    // В GroupHelper не получается установить блок ожидания элемента selected. Где он должен быть, в какой вкладке?
+//  WebElement selected = wait.until(presenceOfElementLocated(By.name("selected[]")));
+
+    //серия действий для входа в систему
+    wd.get(properties.getProperty("web.baseUrl"));
+    }
+
+  public void stop() {
+    wd.quit();
+  }
+
+
+}
---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -0,0 +1,33 @@
+package ru.stqa.pft.mantis.tests;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.stqa.pft.mantis.appmanager.ApplicationManager;
+
+
+
+public class TestBase {
+
+  // Выбор браузера для запусков тестов
+  // драйверы для запуска в папке C:\tools
+  // static самостаятельная глобальная переменная.
+  //browser - парамет позволяет передавать вид браузера для запуска
+  //в конфигурациях запуска прописывать -ea -Dbrowser=chrome, к примеру
+  public static final ApplicationManager app
+          = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
+ // protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser",BrowserType.CHROME));
+ // protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser",BrowserType.IE));
+
+  @BeforeSuite (alwaysRun = true)
+  public void setUp() throws Exception {
+    app.init();
+  }
+
+  @AfterSuite
+  public void tearDown() {
+    app.stop();
+  }
+
+
+}
