447f231c25983f6b9d609afe5b28f15dc0f70a97
chesnokova <chesnokova@sibvaleo.com>
2016-12-15 02:08:20 +0700

    REST: взаимодействие с Bugify

---
+++ rest-sample/src/test/java/ru/stqa/pft/rest/Issue.java
@@ -0,0 +1,41 @@
+package ru.stqa.pft.rest;
+
+/**
+ * Created by chesnokova.sa on 15.12.2016.
+ */
+public class Issue {
+
+
+     private int id;
+     private String subject;
+     private String description;
+
+    public int getId() {
+
+         return id;
+     }
+
+     public Issue withId(int id) {
+         this.id = id;
+         return this;
+     }
+
+     public String getSubject() {
+         return subject;
+     }
+
+     public Issue withSubject(String subject) {
+         this.subject = subject;
+         return this;
+     }
+
+     public String getDescription() {
+         return description;
+     }
+
+     public Issue withDescription(String description) {
+         this.description = description;
+         return this;
+     }
+
+}
